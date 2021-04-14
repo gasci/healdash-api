8e870740dc209d3b20c363c83e515bd40c7b65b6
Göktuğ Aşcı <g.asci@hotmail.com>
2021-04-14 16:33:05 +0300

    whitenoise

--- healdash/settings.py
+++ healdash/settings.py
@@ -54,12 +54,15 @@ INSTALLED_APPS = [
     'django_cleanup'
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
