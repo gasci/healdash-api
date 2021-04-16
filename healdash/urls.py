966a7ef968b6c21c8f54a21466992d42ccf55222
Göktuğ Aşcı <g.asci@hotmail.com>
2021-04-16 10:19:02 +0300

    update

--- healdash/urls.py
+++ healdash/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
 from landing.views import landing_view
@@ -18,11 +19,8 @@ urlpatterns = [
     path('employee/', include('employees.urls')),
     path('account/', include('accounts.urls')),
     path('', landing_view, name='landing_homepage'),
+    path('media/<path>', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    path('static/<path>', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
